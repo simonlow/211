9f5d84e92f61c40a94893cd3e41ad435290e9756
Simon Low <simonlow@live.unc.edu>
2021-04-27 17:05:27 -0400

    Update Str.h

--- include/Str.h
+++ include/Str.h
@@ -1,44 +1,84 @@
-#ifndef STR_VEC_H
-#define STR_VEC_H
+#ifndef STR_H
+#define STR_H
 
 #include "Vec.h"
-#include "Str.h"
 
-/* StrVec is a Vector of Str values. */
-typedef Vec StrVec;
-
-/* Construct an empty StrVec value. Owner is 
- * responsible for calling Str_drop when its 
- * lifetime expires.
+/**
+ * Str is just an alias of Vec to make its purpose of storing
+ * char data and the following functions clear.
  */
-StrVec StrVec_value(size_t capacity);
+typedef Vec Str;
 
-/* Owner of StrVec must call drop to expire
- * its lifetime and the heap memory its Str
- * items own. */
-void StrVec_drop(StrVec *self);
+/**
+ * Construct an empty Str value. Owner is responsible for calling
+ * Str_drop when its lifetime expires.
+ */
+Str Str_value(size_t capacity);
 
-/* Returns the # of Strs in the StrVec */
-size_t StrVec_length(const StrVec *self);
+/**
+ * Owner of a Str must call to expire its buffer data's lifetime.
+ * Frees any heap memory the Str owns.
+ */
+void Str_drop(Str *self);
 
-/* Returns a pointer to a Str at given index.  */
-Str* StrVec_ref(const StrVec *self, size_t index);
+/**
+ * Returns the length of the Str _not_ including the null terminating
+ * character.
+ */
+size_t Str_length(const Str *self);
 
-/* Push a Str value onto the end of the StrVec.
- * The StrVec self becomes the owner of Str value. */
-void StrVec_push(StrVec *self, Str value);
+/**
+ * Returns a pointer to a character at a specific offset. Caller is
+ * free to modify the memory address returned, but not beyond it.
+ */
+char* Str_ref(const Str *self, size_t index);
 
-/* Pop a Str value off the end of the StrVec.
- * After popping, StrVec's length is reduced by one.
- * The popped Str is returned and becomes owned
- * by the caller of StrVec_pop. */
-Str StrVec_pop(StrVec *self);
+/**
+ * Returns a pointer to the first character of the Str's buffer.
+ * Returned value should be used for read purposes only. To mutate
+ * the Str, make use of Str_splice, Str_append, or Str_set.
+ */
+const char* Str_cstr(const Str *self);
 
-/* Set a Str at a specific index. If the index
- * is the length of the StrVec, value will be appended.
- * Otherwise, whatever Str was previously at the
- * index will be dropped and then replaced with value.
- * The StrVec self becomes the owner of Str value. */
-void StrVec_set(StrVec *self, size_t index, Str value);
+/**
+ * Construct a new Str value from a C-string. Str_from will _copy_
+ * the `cstr` contents to a new heap allocated buffer. Caller is 
+ * responsible for calling Str_drop when its lifetime expires.
+ *
+ * Usage: Str s = Str_from("hello, world");
+ */
+Str Str_from(const char *cstr);
+
+/**
+ * Starting from `index`, remove `delete_count` items from `self`,
+ * and insert `insert_count` values from `cstr` at that index of `self`.
+ */
+void Str_splice(
+        Str *self,
+        size_t index,
+        size_t delete_count,
+        const char *cstr,
+        size_t insert_count
+     );
+
+/**
+ * Append a C-string to a Str. Will dynamically grow the Str's 
+ * memory allocation to be able to store the cstr's character
+ * contents if necessary.
+ */
+void Str_append(Str *self, const char *cstr);
+
+/**
+ * Get a character at a specific index of the Str.
+ */
+char Str_get(const Str *self, size_t index);
+
+/**
+ * Set a character at a specific index of the Str. If the index
+ * is the length of the Str, it will append the character to the
+ * end of the Str and terminate with a null character.
+ */
+void Str_set(Str *self, size_t index, const char value);
 
 #endif
+
